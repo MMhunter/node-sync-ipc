fda8d7996cf5f5011eaed11a400eb531e7e65321
mmhunter <mmhuntermax@gmail.com>
2018-03-13 00:11:46 +0800

    emit set parent id for default situation

--- src/basic.h
+++ src/basic.h
@@ -2,6 +2,7 @@
 #define DEBUG 0
 #define MAX_BUFFER_LENGTH 2000
 #ifdef _WIN32
+#include    <tlhelp32.h>
 #else
 #include <unistd.h>
 #include <sys/types.h>
--- src/client.cpp
+++ src/client.cpp
@@ -72,6 +72,39 @@ namespace client {
 #endif
 	}
 
+	int _getppid()
+    {
+#ifdef _WIN32
+        HANDLE hSnapshot;
+        PROCESSENTRY32 pe32;
+        DWORD ppid = 0, pid = GetCurrentProcessId();
+
+        hSnapshot = CreateToolhelp32Snapshot( TH32CS_SNAPPROCESS, 0 );
+        __try{
+            if( hSnapshot == INVALID_HANDLE_VALUE ) __leave;
+
+            ZeroMemory( &pe32, sizeof( pe32 ) );
+            pe32.dwSize = sizeof( pe32 );
+            if( !Process32First( hSnapshot, &pe32 ) ) __leave;
+
+            do{
+                if( pe32.th32ProcessID == pid ){
+                    ppid = pe32.th32ParentProcessID;
+                    break;
+                }
+            }while( Process32Next( hSnapshot, &pe32 ) );
+
+        }
+        __finally{
+            if( hSnapshot != INVALID_HANDLE_VALUE ) CloseHandle( hSnapshot );
+        }
+
+        return ppid;
+#else
+        return getppid();
+#endif
+    }
+
     typedef struct {
         uv_write_t req;
         uv_buf_t buf;
@@ -283,6 +316,9 @@ namespace client {
             if (info[0]->IsNumber()) {
 
                 int parent_pid = (int) Local<Number>::Cast(info[0])->NumberValue();
+                if(parent_pid == 0){
+                    parent_pid = _getppid();
+                }
                 int temp = parent_pid;
                 int parent_pid_digits = 0;
                 while(temp>0){
